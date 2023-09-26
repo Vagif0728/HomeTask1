1292f06df01e27e3749238d95595699030f771e5
Vagif0728 <tu7j4ryqj@code.edu.az>
2023-09-27 01:15:48 +0400

    new task: task 1st and 4th

---
+++ HomeTask1/Program.cs
@@ -0,0 +1,62 @@
+﻿//--------------------------------Task 1(1)---------------------------
+
+//Console.WriteLine("Dordreqemli ededi daxil edin:");
+
+//int num = int.Parse(Console.ReadLine());
+
+//if ((num / 1000 >= 1 || num / 1000 <= -1) && (num/1000<10 || num / 1000 >- 10)) 
+//{
+//    int cem = 0;
+//    int qaliq = 0;
+
+
+//    qaliq = num % 10;
+//    cem = qaliq + cem;
+//    num = num / 10;
+//    qaliq = num % 10;
+//    cem = qaliq + cem;
+//    num = num / 10;
+//    qaliq = num % 10;
+//    cem = qaliq + cem;
+//    qaliq = num / 10;
+//    cem = qaliq + cem;
+
+//    Console.WriteLine(cem);
+//}
+//else
+//{
+//    Console.WriteLine("Eded duzgun deyil.Zehmet olmasa 4 reqemli eded daxil edin");
+//}
+
+//-----------------------------------Task1(2)--------------------------------------
+
+//Console.WriteLine("Dordreqemli ededi daxil edin:");
+
+//int num = int.Parse(Console.ReadLine());
+
+//if ((num / 1000 >= 1) && num / 1000 < 10)
+//{
+//    int cem = 0;
+//    int qaliq = 0;
+
+
+//    qaliq = num % 10;
+//    cem = qaliq + cem;
+//    num = num / 10;
+//    qaliq = num % 10;
+//    cem = qaliq + cem;
+//    num = num / 10;
+//    qaliq = num % 10;
+//    cem = qaliq + cem;
+//    qaliq = num / 10;
+//    cem = qaliq + cem;
+
+//    Console.WriteLine(cem);
+//}
+//else
+//{
+//    Console.WriteLine("Eded duzgun deyil.Zehmet olmasa 4 reqemli eded daxil edin");
+//}
+
+
+
